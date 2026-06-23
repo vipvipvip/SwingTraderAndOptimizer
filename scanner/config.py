aa522f62e6e03599b392cfd28ff23c19dc50ae32
Dikesh <dikeshchokshi@gmail.com>
2026-06-23 11:06:15 -0400

    + Fixes to services + removal of some symlinks

--- scanner/config.py
+++ scanner/config.py
@@ -1,1 +1,32 @@
-services/config.py
+import os
+from dotenv import load_dotenv
+import psycopg2
+
+load_dotenv(os.path.join(os.path.dirname(__file__), 'backend', '.env'))
+
+API_KEY = os.getenv('ALPACA_API_KEY')
+SECRET_KEY = os.getenv('ALPACA_SECRET_KEY')
+
+DB_CONFIG = {
+    'host': '127.0.0.1',
+    'port': 5432,
+    'database': 'swingtrader',
+    'user': 'swingtrader',
+    'password': 'swingtrader_dev_password',
+}
+
+EMA_FAST = 24
+EMA_SLOW = 52
+MACD_SIGNAL_PERIOD = 18
+PPO_FAST = 12
+PPO_SLOW = 26
+PPO_SIGNAL_PERIOD = 9
+
+ATR_PERIOD = 14
+ATR_MULT = 2.0
+
+TABLE = 'tbl_scanner_tickers'
+
+
+def get_db_conn():
+    return psycopg2.connect(**DB_CONFIG)
